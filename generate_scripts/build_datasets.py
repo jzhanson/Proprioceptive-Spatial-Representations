d59b77262badcb3ff84d05006e440730d48333b6
Emilio Parisotto <eparisot@andrew.cmu.edu>
2018-09-13 18:09:43 -0400

    bugfix + make sure json files per segment configuration are equal

--- generate_scripts/build_datasets.py
+++ generate_scripts/build_datasets.py
@@ -11,7 +11,7 @@ class BuildDatasets:
         self.directory = self.args['directory']
         self.body_type = body_type
         if type(self.args['num_segments']) is list:
-            self.dataset_split = np.random.permutation(range(self.args['num_segments'][0], self.args['num_segments'][1] + 1))
+            self.dataset_split = list(np.random.permutation(range(self.args['num_segments'][0], self.args['num_segments'][1] + 1)))
         else:
             self.dataset_split = self.args['num_segments']
         print(self.dataset_split)
@@ -20,39 +20,49 @@ class BuildDatasets:
         args = copy.deepcopy(self.args)
         args['directory'] = self.directory + '/' + dataset
 
+        num_training_segment_configs = len(self.dataset_split) // 2
+        num_validation_segment_configs = len(self.dataset_split) // 4
+        num_testing_segment_configs = len(self.dataset_split) // 4
+
         # Assume even # of dataset splits
         # TODO(josh): add ways to split datasets on things besides number of body segments
         if dataset == 'train':
             if type(self.dataset_split) is list:
-                for i in range(self.args['num_bodies']):
-                    args['num_segments'] = random.choice(self.dataset_split[0:(len(self.dataset_split) // 2)])
-                    args['num_bodies'] = 1
-                    randomize = RandomizeBodies(self.body_type, args)
-                    randomize.build_bodies()
+                for i in range(0, num_training_segment_configs):
+                    for j in range(self.args['num_bodies']):
+                        #args['num_segments'] = random.choice(self.dataset_split[0:(len(self.dataset_split) // 2)])
+                        args['num_segments'] = self.dataset_split[i]
+                        args['num_bodies'] = 1
+                        randomize = RandomizeBodies(self.body_type, args)
+                        randomize.build_bodies()
             else:
                 randomize = RandomizeBodies(self.body_type, args)
                 randomize.build_bodies()
 
         elif dataset == 'valid':
-            for i in range(self.args['num_bodies']):
-                if type(self.dataset_split) is list:
-                    args['num_segments'] = random.choice(self.dataset_split[len(self.dataset_split) // 2:len(self.dataset_split) // 2 + len(self.dataset_split) // 4])
-                    args['num_bodies'] = 1
-                    randomize = RandomizeBodies(self.body_type, args)
-                    randomize.build_bodies()
-                else:
-                    randomize = RandomizeBodies(self.body_type, args)
-                    randomize.build_bodies()
+            if type(self.dataset_split) is list:
+                for i in range(num_training_segment_configs, num_training_segment_configs+num_validation_segment_configs):
+                    for j in range(self.args['num_bodies']):
+                        #args['num_segments'] = random.choice(self.dataset_split[len(self.dataset_split) // 2:len(self.dataset_split) // 2 + len(self.dataset_split) // 4])
+                        args['num_segments'] = self.dataset_split[i]
+                        args['num_bodies'] = 1
+                        randomize = RandomizeBodies(self.body_type, args)
+                        randomize.build_bodies()
+            else:
+                randomize = RandomizeBodies(self.body_type, args)
+                randomize.build_bodies()
         elif dataset == 'test':
-            for i in range(self.args['num_bodies']):
-                if type(self.dataset_split) is list:
-                    args['num_segments'] = random.choice(self.dataset_split[len(self.dataset_split) // 2 + len(self.dataset_split) // 4:])
-                    args['num_bodies'] = 1
-                    randomize = RandomizeBodies(self.body_type, args)
-                    randomize.build_bodies()
-                else:
-                    randomize = RandomizeBodies(self.body_type, args)
-                    randomize.build_bodies()
+            if type(self.dataset_split) is list:
+                for i in range(num_training_segment_configs+num_validation_segment_configs, len(self.dataset_split)):
+                    for j in range(self.args['num_bodies']):
+                        #args['num_segments'] = random.choice(self.dataset_split[len(self.dataset_split) // 2 + len(self.dataset_split) // 4:])
+                        args['num_segments'] = self.dataset_split[i]
+                        args['num_bodies'] = 1
+                        randomize = RandomizeBodies(self.body_type, args)
+                        randomize.build_bodies()
+            else:
+                randomize = RandomizeBodies(self.body_type, args)
+                randomize.build_bodies()
 
 if __name__ == '__main__':
     body_type = sys.argv[1] if len(sys.argv) > 1 else 'BipedalWalker'
